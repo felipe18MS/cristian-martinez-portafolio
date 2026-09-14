8fc2ada149c399370503c41b286cb9f404f098c7
felipe18MS <cristianmartinez4002@gmail.com>
2026-09-14 00:48:44 -0500

    ci: deploy portfolio to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  base: '/cristian-martinez-portafolio/',
+
   test: {
     globals: true,
     environment: 'jsdom',
